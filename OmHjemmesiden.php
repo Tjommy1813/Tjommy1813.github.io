501615e45ef51d901306d99d97bd7b014975c142
Tjommy1813 <104358465+Tjommy1813@users.noreply.github.com>
2022-05-05 22:17:33 +0200

    Add files via upload

---
+++ OmHjemmesiden.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html> 
+    <style>
+        body {
+            background: url(https://inspirationhut.net/wp-content/uploads/2013/05/201.png);
+        }
+        .information {
+            width: 600px;
+            margin: auto;
+            margin-top: 30px;
+            padding: 10px;
+            font-size: 20px;
+            /*
+            border-width: 10px;
+            border-color: hsl(180, 80%, 25%);
+            border-style: solid;
+            border-radius: 10px;
+            background-color: hsl(180, 90%, 35%);
+            */
+        }
+        h2 {
+            font-size: 40px;
+            text-align: center;
+        }
+    </style>
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/hjemmeside/assets/php/"; include($IPATH."header.html"); ?>
+    
+    <body>
+        <div class="information">
+            <head>
+                <h2>Om Tjommys hjemmeside</h2>
+                <title>Om hjemmesiden</title>
+            </head>
+            <p>Denne hjemmeside er lavet at mig. Jeg har helt selv skrevet den fra bunden i html med css og lidt php. Hjemmesiden bliver hosted igenem cloudflare. Hjemmesiden er at fritidsprojekt og skal blive til en slags logbog over alle de projekter jeg får lavet gennem tiden.</p>
+            <p>Klager og forslag til hjemmesiden kan sendes til: BilligSkidt@skaldespand.dk</p>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        body {
+            font-family: roboto;
+            background: url(https://inspirationhut.net/wp-content/uploads/2013/05/201.png);
+        }
+        h2 {
+            font-size: 150px;
+            text-align: center;
+        }
+        h3 {
+            font-size: 20px;
+            margin-top: -130px;
+            text-align: center;
+        }
+    </style>
+    <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/hjemmeside/assets/php/"; include($IPATH."header.html"); ?>
+    <title>Tjommys Hjemmeside</title>
+    <body>
+        <h2>Welcommen</h1>
+        <h3>Til hvad der nu end er lavet</h2>
+    </body>
+</html>
